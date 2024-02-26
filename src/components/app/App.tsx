8d4d8dc2979697524c7b6cfa5ecc7761cb2cdc75
FAbrickA <schilonosov.alex@yandex.ru>
2024-02-26 23:36:59 +0300

    M2 hw1

---
+++ src/components/app/App.tsx
@@ -0,0 +1,9 @@
+import MainScreen from '../../pages/main-screen/MainScreen';
+
+const placeCardNumber = 5;
+
+export default function App() {
+  return (
+    <MainScreen placeCardNumber={placeCardNumber}/>
+  );
+}
